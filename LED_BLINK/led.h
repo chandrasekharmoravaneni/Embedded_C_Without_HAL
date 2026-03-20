7f5a01e5120369d02011691da0498c5fa283e454
chandrasekharmoravaneni <chandrasekhar.moravaneni.naidu@gmail.com>
2026-03-20 17:46:27 +0100

    added hardware initialization of gpioa port details

--- LED_BLINK/led.h
+++ LED_BLINK/led.h
@@ -3,6 +3,16 @@
 #include "stm32f4xx.h"
 #include "uart.h"
 
+#define LED_PORT GPIOB
+#define LED_PORT_CLK_EN (1U << 1) // Enable clock for GPIOB
+#define LED_PIN_RED (1U << 14) // Pin 14 for Red LED
+#define LED_PIN_GREEN (1U << 12) // Pin 12 for Green LED
+#define LED_PIN_BLUE (1U << 13) // Pin 13 for Blue LED
+
+#define LED_RED_MODE_BIT (1U << 28) // Mode bit for Red LED (Pin 14)
+#define LED_GREEN_MODE_BIT (1U << 24) // Mode bit for Green LED (Pin 12)
+#define LED_BLUE_MODE_BIT (1U << 26) // Mode bit for Blue LED (Pin 13)
+
 typedef enum {
     RED = 0,
     GREEN = 1,
